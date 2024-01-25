0035b0119e9d28c07c066bcb568f8cb95995a6ab
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-25 15:59:31 +0100

    Add the "tiles" and "onTileClick" arguments

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import Tiles from '../Tile/Tile'
+import Tile from '../Tile/Tile'
 import Strike from '../Strike/Strike'
 import './Board.css'
 
-function Board() {
+function Board({ tiles, onTileClick }) {
   return (
     <div className="board">
-      <Tiles />
+      <div className="row">
+        <Tile onClick={() => onTileClick(0)} value={tiles[0]} />
+        <Tile onClick={() => onTileClick(1)} value={tiles[1]} />
+        <Tile onClick={() => onTileClick(2)} value={tiles[2]} />
+      </div>
+      <div className="row">
+        <Tile onClick={() => onTileClick(3)} value={tiles[3]} />
+        <Tile onClick={() => onTileClick(4)} value={tiles[4]} />
+        <Tile onClick={() => onTileClick(5)} value={tiles[5]} />
+      </div>
+      <div className="row">
+        <Tile onClick={() => onTileClick(6)} value={tiles[6]} />
+        <Tile onClick={() => onTileClick(7)} value={tiles[7]} />
+        <Tile onClick={() => onTileClick(8)} value={tiles[8]} />
+      </div>
+
       <Strike />
     </div>
   )
