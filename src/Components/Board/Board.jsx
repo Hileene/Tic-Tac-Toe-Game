91efc7dc64a3686d03a846a7be95b0a9ad9c6ba6
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-26 13:05:37 +0100

    Add properties 'playerTurn' and 'strikeClass'

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -3,26 +3,62 @@ import Tile from '../Tile/Tile'
 import Strike from '../Strike/Strike'
 import './Board.css'
 
-function Board({ tiles, onTileClick }) {
+function Board({ tiles, onTileClick, playerTurn, strikeClass }) {
   return (
     <div className="board">
       <div className="row">
-        <Tile onClick={() => onTileClick(0)} value={tiles[0]} />
-        <Tile onClick={() => onTileClick(1)} value={tiles[1]} />
-        <Tile onClick={() => onTileClick(2)} value={tiles[2]} />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(0)}
+          value={tiles[0]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(1)}
+          value={tiles[1]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(2)}
+          value={tiles[2]}
+        />
       </div>
       <div className="row">
-        <Tile onClick={() => onTileClick(3)} value={tiles[3]} />
-        <Tile onClick={() => onTileClick(4)} value={tiles[4]} />
-        <Tile onClick={() => onTileClick(5)} value={tiles[5]} />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(3)}
+          value={tiles[3]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(4)}
+          value={tiles[4]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(5)}
+          value={tiles[5]}
+        />
       </div>
       <div className="row">
-        <Tile onClick={() => onTileClick(6)} value={tiles[6]} />
-        <Tile onClick={() => onTileClick(7)} value={tiles[7]} />
-        <Tile onClick={() => onTileClick(8)} value={tiles[8]} />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(6)}
+          value={tiles[6]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(7)}
+          value={tiles[7]}
+        />
+        <Tile
+          playerTurn={playerTurn}
+          onClick={() => onTileClick(8)}
+          value={tiles[8]}
+        />
       </div>
 
-      <Strike />
+      <Strike strikeClass={strikeClass} />
     </div>
   )
 }
