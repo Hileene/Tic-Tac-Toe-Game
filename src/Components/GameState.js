c7ba13540442efaa11b554f65ee3c2163d3d0389
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-26 15:09:16 +0100

    Implementing the game state of the GameOver component

---
+++ src/Components/GameState.js
@@ -0,0 +1,10 @@
+//This component is similar to an enumeration(enum) in other languages
+
+const GameState = {
+  playerXWins: 0,
+  playerOWins: 1,
+  draw: 2,
+  inProgress: 3,
+}
+
+export default GameState
